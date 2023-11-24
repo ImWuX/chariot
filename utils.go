17e79222cb64c40a5b7930f2a926d8f5bc13d5b4
WuX <axel.riska@gmail.com>
2023-11-24 13:32:36 +0100

    Major rework. Again. This commit significantly rolls back the progress of chariot but chariot has to be reworked. It is commited to main as chariot is not used in production and so breaking main is ok for now.

--- utils.go
+++ utils.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
-	"path/filepath"
+	"regexp"
 	"slices"
 	"strings"
 )
@@ -13,73 +13,52 @@ func FileExists(path string) bool {
 	return err == nil
 }
 
-func ParseTag(tag string) (string, string) {
-	tagParts := strings.Split(tag, ":")
-	if len(tagParts) > 1 {
-		return tagParts[1], tagParts[0]
-	}
-	return tagParts[0], ""
-}
-
-func MakeTag(tag string, tagType string) string {
-	return fmt.Sprintf("%s:%s", tagType, tag)
-}
-
 func ArrIncludes(arr []string, str string) bool {
 	return slices.ContainsFunc(arr, func(e string) bool {
 		return e == str
 	})
 }
 
-type ChariotCache string
-
-func (cache ChariotCache) Path() string {
-	return string(cache)
+type Tag struct {
+	id   string
+	kind string
 }
 
-func (cache ChariotCache) Container() string {
-	return filepath.Join(cache.Path(), "container")
-}
-
-func (cache ChariotCache) Sysroot() string {
-	return filepath.Join(cache.Path(), "root")
-}
-
-func (cache ChariotCache) Sources() string {
-	return filepath.Join(cache.Path(), "sources")
-}
-
-func (cache ChariotCache) Source(tag string) string {
-	return filepath.Join(cache.Sources(), tag)
-}
-
-func (cache ChariotCache) Builds(host bool) string {
-	sub := "build"
-	if host {
-		sub = "host-build"
+func CreateTag(id string, kind string) (Tag, error) {
+	tag := Tag{id: id, kind: kind}
+	if tag.kind != "" && tag.kind != "host" && tag.kind != "source" {
+		return Tag{}, fmt.Errorf("invalid tag kind (%s)", tag.kind)
 	}
-	return filepath.Join(cache.Path(), sub)
+	if r, _ := regexp.Compile(`^[a-z\-1-9]+$`); !r.Match([]byte(tag.id)) {
+		return Tag{}, fmt.Errorf("tag id (%s) contains invalid characters", tag.id)
+	}
+	return tag, nil
 }
 
-func (cache ChariotCache) Build(tag string, host bool) string {
-	return filepath.Join(cache.Builds(host), tag)
+func StringToTag(tag string) (Tag, error) {
+	tagParts := strings.Split(tag, ":")
+	if len(tagParts) > 1 {
+		return CreateTag(tagParts[1], tagParts[0])
+	} else {
+		return CreateTag(tagParts[0], "")
+	}
 }
 
-func (cache ChariotCache) Init() error {
-	if err := os.MkdirAll(cache.Path(), 0755); err != nil {
-		return err
+func StringsToTags(tags []string) ([]Tag, error) {
+	ntags := make([]Tag, 0)
+	for _, tag := range tags {
+		ntag, err := StringToTag(tag)
+		if err != nil {
+			return nil, err
+		}
+		ntags = append(ntags, ntag)
 	}
-	if err := os.MkdirAll(cache.Sysroot(), 0755); err != nil {
-		return err
+	return ntags, nil
+}
+
+func (tag Tag) ToString() string {
+	if tag.kind == "" {
+		return tag.id
 	}
-	if err := os.MkdirAll(cache.Sources(), 0755); err != nil {
-		return err
-	}
-	if err := os.MkdirAll(cache.Builds(false), 0755); err != nil {
-		return err
-	}
-	if err := os.MkdirAll(cache.Builds(true), 0755); err != nil {
-		return err
-	}
-	return nil
+	return fmt.Sprintf("%s:%s", tag.kind, tag.id)
 }
